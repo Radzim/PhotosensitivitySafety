c9dd781d4760387ac2bc1dbc7026d894ac514986
Radzim <57621493+Radzim@users.noreply.github.com>
2021-10-25 17:42:45 +0100

    proposal changes, back to python

--- Python/justRL.py
+++ Python/justRL.py
@@ -1,5 +1,3 @@
-import datetime
-
 import cv2
 import numpy as np
 import pafy
@@ -51,16 +49,10 @@ while True:
 
         relativeLuminance = np.dot(X[..., :3], [0.0722, 0.7152, 0.2126])
 
-        redSaturation = np.divide(np.dot(X[..., :3], [0, 0, 1]), np.dot(X[..., :3], [1, 1, 1]))
-        redMajority = np.minimum(np.multiply(np.dot(X[..., :3], [-1, -1, 1]), 320), 0)
-
         deltaRelativeLuminance = relativeLuminance-previousRelativeLuminance
 
         limitRelativeLuminanceLighter = np.array(np.multiply(np.where(deltaRelativeLuminance >= 0.1, 1, 0), np.where(np.minimum(relativeLuminance, previousRelativeLuminance) <= 0.8, 1, 0)), dtype=np.uint8)
         limitRelativeLuminanceDarker = np.array(np.multiply(np.where(deltaRelativeLuminance <= -0.1, 1, 0), np.where(np.minimum(relativeLuminance, previousRelativeLuminance) <= 0.8, 1, 0)), dtype=np.uint8)
-
-        deltaRedMajority = np.array(np.maximum(redMajority-previousRedMajority, previousRedMajority-redMajority))
-        limitRedMajority = np.array(np.multiply(np.where(deltaRedMajority >= 20, 1, 0), np.where(np.maximum(redSaturation, previousRedSaturation) >= 0.8, 1, 0)), dtype=np.uint8)
 
         # SHOW MONITORS
         if displayAnalysis:
@@ -69,33 +61,21 @@ while True:
 
             cv2.imshow('Original', X8bit)
 
-            #relativeLuminance8bit = np.array(np.multiply(relativeLuminance, 255), dtype=np.uint8)
-            #cv2.imshow('Relative Luminance', relativeLuminance8bit)
-
-            #deltaRelativeLuminance8bit = np.array(np.multiply(deltaRelativeLuminance, 255), dtype=np.uint8)
-            #cv2.imshow('Relative Luminance Delta', deltaRelativeLuminance8bit)
-
             limitRelativeLuminance8bitLighter = np.array(np.multiply(limitRelativeLuminanceLighter, 255), dtype=np.uint8)
             cv2.imshow('Lighter Relative Luminance Delta Breach', limitRelativeLuminance8bitLighter)
             limitRelativeLuminance8bitDarker = np.array(np.multiply(limitRelativeLuminanceDarker, 255), dtype=np.uint8)
             cv2.imshow('Darker Relative Luminance Delta Breach', limitRelativeLuminance8bitDarker)
 
-            limitRedMajority8bit = np.array(np.multiply(limitRedMajority, 255), dtype=np.uint8)
-            cv2.imshow('Red Majority Delta Breach', limitRedMajority8bit)
-
             # SHOW PLOTS
 
             luminanceLimitPlotDarker.plot(np.average(limitRelativeLuminance8bitDarker), label="Darker", line=0)
             luminanceLimitPlotLighter.plot(np.average(limitRelativeLuminance8bitLighter), label="Lighter", line=0)
-            redLimitPlot.plot(np.average(limitRedMajority8bit)*10, label="Red Flash", line=0)
 
             cv2.waitKey(10)
 
         # REMEMBER LAST FRAME
 
         previousRelativeLuminance = relativeLuminance
-        previousRedMajority = redMajority
-        previousRedSaturation = redSaturation
 
         continue
     break
