d96d5a15496c765981a7a17b22beaa2d89bd784b
Radzim <57621493+Radzim@users.noreply.github.com>
2021-11-23 16:24:30 +0000

    kivy shape

--- Python/kivyapp.py
+++ Python/kivyapp.py
@@ -1,101 +1,89 @@
-import kivy
-
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.lang import Builder
-
-from kivy.uix.button import Button
-from kivy.uix.filechooser import FileChooserListView
-
 import os
-
-
-
-
-class MyWidget(BoxLayout):
-
-    def __init__(self, **kwargs):
-        super(MyWidget, self).__init__(**kwargs)
-
-        container = BoxLayout(orientation='vertical')
-
-        filechooser = FileChooserListView()
-        filechooser.bind(on_selection=lambda x: self.selected(filechooser.selection))
-
-        open_btn = Button(text='open', size_hint=(1, .2))
-        open_btn.bind(on_release=lambda x: self.open(filechooser.path, filechooser.selection))
-
-        container.add_widget(filechooser)
-        container.add_widget(open_btn)
-        self.add_widget(container)
-
-    def open(self, path, filename):
-        if len(filename) > 0:
-            print(filename)
-            print(os.path.join(path, filename[0]))
-
-    # def selected(self, filename):
-    #     print("selected: %s" % filename[0])
-
-
-# class MyApp(App):
-#     def build(self):
-#         return MyWidget()
-#
-#
-# if __name__ == '__main__':
-#     MyApp().run()
-#
-
-import kivy
+from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
+
 
 class InterfaceManager(BoxLayout):
 
     def __init__(self, **kwargs):
         super(InterfaceManager, self).__init__(**kwargs)
 
-        self.first = Button(text="First")
-        container = BoxLayout(orientation='vertical')
-        file_btn = Button(text='Open File', size_hint=(1, .2))
-        file_btn.bind(on_release=self.show_second)
-        capture_btn = Button(text='Open File', size_hint=(1, .2))
-        capture_btn.bind(on_release=self.show_second)
-        container.add_widget(file_btn)
-        self.first = container
+        # BUTTONS:
+        file_btn = Button(text='Open File', size_hint=(.2, .2))
+        file_btn.bind(on_release=self.show_file_open)
 
-        container = BoxLayout(orientation='vertical')
+        capture_btn = Button(text='Live Capture', size_hint=(.2, .2))
+        capture_btn.bind(on_release=self.show_capture)
+
+        back_btn1 = Button(text='back', size_hint=(.2, .2))
+        back_btn1.bind(on_release=self.show_first)
+
+        back_btn2 = Button(text='back', size_hint=(.2, .2))
+        back_btn2.bind(on_release=self.show_first)
+
+        open_btn = Button(text='open', size_hint=(.2, .2))
+        open_btn.bind(on_release=lambda x: self.open(filechooser.path, filechooser.selection))
+
+        # OTHER ELEMENTS:
+
         filechooser = FileChooserListView()
         filechooser.bind(on_selection=lambda x: self.selected(filechooser.selection))
-        open_btn = Button(text='open', size_hint=(1, .2))
-        open_btn.bind(on_release=lambda x: self.open(filechooser.path, filechooser.selection))
-        container.add_widget(filechooser)
-        container.add_widget(open_btn)
-        self.second = container
-        self.second.bind(on_press=self.show_final)
 
-        self.final = Label(text="Hello World")
+        # CONTAINERS
+
+        # main screen
+        container = BoxLayout(orientation='vertical')
+        container.add_widget(file_btn)
+        container.add_widget(capture_btn)
+        self.first = container
+
+        # open file
+        container = BoxLayout(orientation='vertical')
+        container.add_widget(filechooser)
+        container.add_widget(back_btn1)
+        container.add_widget(open_btn)
+        self.file_open = container
+
+        # capture screen
+        container = BoxLayout(orientation='vertical')
+        container.add_widget(back_btn2)
+        self.capture = container
+
+        # end screen
+        self.final = Label(text="END")
         self.add_widget(self.first)
 
-    def show_second(self, button):
+    # SWITCH VIEWS
+
+    def show_first(self, button):
         self.clear_widgets()
-        self.add_widget(self.second)
+        self.add_widget(self.first)
+
+    def show_file_open(self, button):
+        self.clear_widgets()
+        self.add_widget(self.file_open)
+
+    def show_capture(self, button):
+        self.clear_widgets()
+        self.add_widget(self.capture)
 
     def show_final(self, button):
         self.clear_widgets()
         self.add_widget(self.final)
+
+    # ACTIONS
 
     def open(self, path, filename):
         if len(filename) > 0:
             print(filename)
             print(os.path.join(path, filename[0]))
 
-class MyApp(App):
+class PhotosensitivitySafetyApp(App):
     def build(self):
         return InterfaceManager(orientation='vertical')
 
 if __name__ == '__main__':
-    MyApp().run()
+    PhotosensitivitySafetyApp().run()
