c08b260c8c317afe0017fe7d96564dcc7de602fb
Radzim <57621493+Radzim@users.noreply.github.com>
2022-04-13 14:58:11 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='photosensitivity_safety',
+    version=1,
+
+    url='https://github.com/Radzim/PhotosensitivitySafety',
+    author='Radzim Sendyka',
+    author_email='radzimsendyka@gmail.com',
+
+    py_modules=['PhotosensitivitySafetyEngine'],
+)
