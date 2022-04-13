587ab5ecb8022ac2be0a9ac6b29ed5a94c849f47
Radzim <57621493+Radzim@users.noreply.github.com>
2022-04-13 15:38:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PhotosensitivitySafety',
-    version="1.0.0",
+    version="1.0.1",
 
     url='https://github.com/Radzim/PhotosensitivitySafety',
     author='Radzim Sendyka',
