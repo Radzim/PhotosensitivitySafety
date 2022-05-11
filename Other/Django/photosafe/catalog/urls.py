b27c96eb34f0dfdca0eb2ed5ddc55128a10b7f63
Radzim <57621493+Radzim@users.noreply.github.com>
2022-05-11 22:14:41 +0100

    removing libraries

--- Other/Django/photosafe/catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-from django.conf.urls import url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('photosafe.urls'))
-]
